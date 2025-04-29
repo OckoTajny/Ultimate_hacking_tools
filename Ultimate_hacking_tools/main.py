8d9c49b04196edcc673d8515aca8d5e3fc53a85d
OckoTajny <88657303+OckoTajny@users.noreply.github.com>
2025-04-29 16:29:43 +0200

    Add files via upload

---
+++ Ultimate_hacking_tools/main.py
@@ -0,0 +1,35 @@
+import os
+import colorama
+
+green = colorama.Fore.GREEN
+red = colorama.Fore.RED
+yellow = colorama.Fore.YELLOW
+blue = colorama.Fore.BLUE
+magenta = colorama.Fore.MAGENTA
+cyan = colorama.Fore.CYAN
+white = colorama.Fore.WHITE
+
+print(magenta + """
+ _   _ _ _   _                 _       
+| | | | | |_(_)_ __ ___   __ _| |_ ___ 
+| | | | | __| | '_ ` _ \ / _` | __/ _ \
+| |_| | | |_| | | | | | | (_| | ||  __/
+ \___/|_|\__|_|_| |_| |_|\__,_|\__\___|
+| |__   __ _  ___| | _(_)_ __   __ _   
+| '_ \ / _` |/ __| |/ / | '_ \ / _` |  
+| | | | (_| | (__|   <| | | | | (_| |  
+|_| |_|\__,_|\___|_|\_\_|_| |_|\__, |  
+| |_ ___   ___ | |___          |___/   
+| __/ _ \ / _ \| / __|                 
+| || (_) | (_) | \__ \                 
+ \__\___/ \___/|_|___/                 
+""")
+print(green + """
+1. DDOS attack""")
+while True:
+    a = input(magenta + "Choose a number: ")
+    if a == "1":
+        os.system("ddos.py")
+    else:
+        print(red + f"{a} is not a valid number.")
+        continue
