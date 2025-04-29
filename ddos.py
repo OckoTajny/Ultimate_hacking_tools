38ac65762d739634d015434ffbff970dd8ddc3e4
OckoTajny <o2t2j2s2@gmail.com>
2025-04-29 20:49:51 +0200

    ascii art

--- ddos.py
+++ ddos.py
@@ -2,10 +2,20 @@ import requests
 import colorama
 import os
 
+from Ultimate_hacking_tools.main import magenta
 
 green = colorama.Fore.GREEN
 red = colorama.Fore.RED
+magenta = colorama.Fore.MAGENTA
 
+os.system("cls" if os.name == "nt" else "clear")
+print(magenta + """
+____  ____   ___  ____       _  _____ _____  _    ____ _  __
+|  _ \|  _ \ / _ \/ ___|     / \|_   _|_   _|/ \  / ___| |/ /
+| | | | | | | | | \___ \    / _ \ | |   | | / _ \| |   | ' / 
+| |_| | |_| | |_| |___) |  / ___ \| |   | |/ ___ \ |___| . \ 
+|____/|____/ \___/|____/  /_/   \_\_|   |_/_/   \_\____|_|\_\
+""")
 url = input(green + "Enter the URL to attack: ")
 method = input(green + "Enter the HTTP method (GET/POST/FETCH): ").upper()
 numero = input(green + "Enter the number of requests to send: ")
@@ -36,4 +46,4 @@ try:
     else:
         print(red + f"Server anwsered with error {response.status_code}")
 except requests.exceptions.RequestException as e:
-    print(green + f"ddos attack worked!!! server error:  {e}")
+    print(green + f"DDOS attack worked!!! server error:  {e}")
