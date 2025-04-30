6ccfd30588177574fcd46f589e547500f57ea65e
Daniel Černý <dandul.black@gmail.com>
2025-04-30 18:19:44 +0200

    ez ascii art + new workspace

---
+++ crypter.py
@@ -0,0 +1,19 @@
+import colorama
+import os
+import time
+
+green = colorama.Fore.GREEN
+red = colorama.Fore.RED
+magenta = colorama.Fore.MAGENTA
+
+os.system("cls" if os.name == "nt" else "clear")
+print(green + """
+ _____                      _               
+/  __ \                    | |              
+| /  \/ _ __  _   _  _ __  | |_   ___  _ __ 
+| |    | '__|| | | || '_ \ | __| / _ \| '__|
+| \__/\| |   | |_| || |_) || |_ |  __/| |   
+ \____/|_|    \__, || .__/  \__| \___||_|   
+               __/ || |                     
+              |___/ |_|                     
+""")
